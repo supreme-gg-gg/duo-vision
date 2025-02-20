30c5f74015cf5f46ddf4eb08ab137bace70eb4b1
Jet Chiang <jetjiang.ez@gmail.com>
2025-02-20 16:30:03 -0500

    wifi debug

--- glasses/src/main.cpp
+++ glasses/src/main.cpp
@@ -14,6 +14,23 @@ void setup() {
     Serial.begin(115200);
     myServo.attach(4);
     myServo2.attach(5);
+
+    Serial.println("Scanning for networks...");
+    int numNetworks = WiFi.scanNetworks();
+
+    if (numNetworks == 0) {
+        Serial.println("No networks found.");
+    } else {
+        Serial.println("Networks found:");
+        for (int i = 0; i < numNetworks; ++i) {
+        Serial.print(i + 1);
+        Serial.print(": ");
+        Serial.print(WiFi.SSID(i));
+        Serial.print(" (");
+        Serial.print(WiFi.RSSI(i)); // Signal strength
+        Serial.println(" dBm)");
+        }
+    }
     
     // Connect to Wi-Fi
     WiFi.begin(ssid, password);
